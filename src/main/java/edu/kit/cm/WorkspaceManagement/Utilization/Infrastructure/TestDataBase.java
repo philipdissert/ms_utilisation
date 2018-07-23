297d51cb372bf695d8a35f34c2ed54c2d95ac23f
Philip <philip.dissert@gmail.com>
2018-07-23 17:16:34 +0200

    initial TestDataBase

--- src/main/java/edu/kit/cm/WorkspaceManagement/Utilization/Infrastructure/TestDataBase.java
+++ src/main/java/edu/kit/cm/WorkspaceManagement/Utilization/Infrastructure/TestDataBase.java
@@ -1,4 +1,13 @@
 package edu.kit.cm.WorkspaceManagement.Utilization.Infrastructure;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class TestDataBase {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
 }

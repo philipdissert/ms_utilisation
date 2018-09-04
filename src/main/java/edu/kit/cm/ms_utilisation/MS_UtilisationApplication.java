7848adf2cef78f0bc4429fdba363ca17ebf916f2
steffen <steffen.schmitt@student.kit.edu>
2018-09-04 12:43:10 +0200

    Rename

--- src/main/java/edu/kit/cm/ms_utilisation/MS_UtilisationApplication.java
+++ src/main/java/edu/kit/cm/ms_utilisation/MS_UtilisationApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MS_ReservationApplication {
+public class MS_UtilisationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MS_ReservationApplication.class, args);
+		SpringApplication.run(MS_UtilisationApplication.class, args);
 	}
 }
